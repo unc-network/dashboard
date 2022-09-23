17d2e7a33e250df0ec250b7e87becc590b2717bd
Will Whitaker <will.whitaker@unc.edu>
2022-09-23 09:05:51 -0400

    notification widget

--- akips/views.py
+++ akips/views.py
@@ -18,7 +18,7 @@ from django.db.transaction import atomic, non_atomic_requests
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
-from .models import Summary, Unreachable, Device, SNMPTrap
+from .models import Summary, Unreachable, Device, SNMPTrap, UserAlert
 from .forms import IncidentForm
 from .task import example_task
 from .utils import AKIPS, ServiceNow, pretty_duration
@@ -52,6 +52,8 @@ class Home(LoginRequiredMixin, View):
 
     def get(self, request, *args, **kwargs):
         context = {}
+
+        context['user_alerts'] = UserAlert.objects.all()
 
         context['critical'] = Summary.objects.filter(type='Critical',status='Open').order_by('name')
         context['tiers'] = Summary.objects.filter(type='Distribution',status='Open').order_by('name')
