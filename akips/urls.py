d28629ea0ed69d4b86f670832e96db2be8e53817
Will Whitaker <will.whitaker@unc.edu>
2022-09-28 13:57:54 -0400

    default tab on closed events

--- akips/urls.py
+++ akips/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     path('incident/', views.IncidentView.as_view(), name='incident'),
 
     # Event Focus Views
+    path('summary/<id>/', views.SummaryView.as_view(), name='summary'),
     path('tier/<tier>/', views.TierView.as_view(), name='tier'),
     path('builiding/<bldg>/', views.BuildingView.as_view(), name='building'),
     path('device/<name>/', views.DeviceView.as_view(), name='device'),
     path('trap/<trap_id>/', views.TrapView.as_view(), name='trap'),
-    path('summary/<id>/', views.SummaryView.as_view(), name='summary'),
     path('recent/', views.RecentSummaryView.as_view(), name='recent'),
     path('recent/traps', views.RecentTrapsView.as_view(), name='recent_traps'),
 
@@ -24,12 +24,11 @@ urlpatterns = [
 
     # API Update Views
     path('api/set_maintenance_mode', views.SetMaintenanceView.as_view(), name='set_maintenance'),
-    path('ack/<summary_id>', views.AckView.as_view(), name='ack'),
-    path('trap/<trap_id>/ack', views.AckTrapView.as_view(), name='ack_trap'),
-    path('trap/<trap_id>/clear', views.ClearTrapView.as_view(), name='clear_trap'),
-    path('chart/', views.ChartDataView.as_view(), name='chart_data'),
-
-    path('preferences/', views.SetUserProfileView.as_view(), name='profile_api'),
+    path('api/summary/<summary_id>/ack', views.AckView.as_view(), name='ack'),
+    path('api/trap/<trap_id>/ack', views.AckTrapView.as_view(), name='ack_trap'),
+    path('api/trap/<trap_id>/clear', views.ClearTrapView.as_view(), name='clear_trap'),
+    path('api/chart/', views.ChartDataView.as_view(), name='chart_data'),
+    path('api/profile/', views.SetUserProfileView.as_view(), name='profile_api'),
 
     # Hibernation Request
     #path('hibernation/', views.HibernationView.as_view(), name='hibernation'),
