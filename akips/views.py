d87ad4882ab57ab1ad85181c58f2b58d7b9e295d
Will Whitaker <will.whitaker@unc.edu>
2022-09-29 13:03:02 -0400

    testing error pages

--- akips/views.py
+++ akips/views.py
@@ -33,23 +33,20 @@ logger = logging.getLogger(__name__)
 
 # Create your views here.
 
-# def handler403(request, exception):
-#     response = render(request, "dashboard/error_pages/403.html")
-#     response.status_code = 403
+def handler403(request, exception):
+    response = render(request, "akips/403.html")
+    response.status_code = 403
+    return response
 
-#     return response
+def handler404(request, exception):
+    response = render(request, "akips/404.html")
+    response.status_code = 404
+    return response
 
-# def handler404(request, exception):
-#     response = render(request, "dashboard/error_pages/404.html")
-#     response.status_code = 404
-
-#     return response
-
-# def handler500(request):
-#     response = render(request, "dashboard/error_pages/500.html")
-#     response.status_code = 500
-
-#     return response
+def handler500(request):
+    response = render(request, "akips/500.html")
+    response.status_code = 500
+    return response
 
 # def login(request, *args, **kwargs):
 #     if request.method == 'POST':
