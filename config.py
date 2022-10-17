e4563622f61eb11361b84d7d99fcad60be29ef49
Will Whitaker <will.whitaker@unc.edu>
2022-10-17 13:30:43 -0400

    comments

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-# configuration file for gunicorn
+# Gunicorn configuration file
+# https://docs.gunicorn.org/en/stable/configure.html#configuration-file
+# https://docs.gunicorn.org/en/stable/settings.html
 import os
 
 # This is needed to extend the worker thread timeout for long running tasks
