4a46b4835e44c757656dc68bb0ea2f1f89cd15e1
Will Whitaker <will.whitaker@unc.edu>
2023-04-04 12:38:35 -0400

    spelling and functions

--- akips/static/akips/js/ocnes.js
+++ akips/static/akips/js/ocnes.js
@@ -1,5 +1,22 @@
 // Basic functions for the OCNES site.
 
+// Get the token to support AJAX POST
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
 function refresh_alerts() {
     // Periodically check for user alerts.
     var refresh_seconds = 30000;
