9d7c29f0928e2773baac5886edfa1be0747dce39
Will Whitaker <will.whitaker@unc.edu>
2023-08-04 12:31:24 -0400

    custom error pages

--- akips/views.py
+++ akips/views.py
@@ -43,10 +43,10 @@ logger = logging.getLogger(__name__)
 #     response.status_code = 403
 #     return response
 
-# def handler404(request, exception):
-#     response = render(request, "akips/404.html")
-#     response.status_code = 404
-#     return response
+def custom_404(request, exception):
+    response = render(request, "akips/error_404.html")
+    response.status_code = 404
+    return response
 
 # def handler500(request):
 #     response = render(request, "akips/500.html")
