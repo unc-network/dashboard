d9697628d6f5c6cf1c9ded31806f935a1bc577b4
William E Whitaker <will.whitaker@unc.edu>
2024-11-05 07:42:16 -0500

    use flow for ticket creation

--- akips/views.py
+++ akips/views.py
@@ -527,8 +527,8 @@ class CreateIncidentView(LoginRequiredMixin, View):
                     context['create_message'] = "Incident {} was associated.".format(incident['ID'])
             else:
                 # Get a new Incident
-                # incident = tdx.create_ticket_flow(
-                incident = tdx.create_ticket(
+                # incident = tdx.create_ticket(
+                incident = tdx.create_ticket_flow(
                     form.cleaned_data.get('assignment_group'),
                     form.cleaned_data.get('criticality'),
                     form.cleaned_data.get('description'),
