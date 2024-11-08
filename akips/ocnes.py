3a796e74259a56d52f6266748d2024edef937427
William E Whitaker <will.whitaker@unc.edu>
2024-11-08 14:59:17 -0500

    debug logging

--- akips/ocnes.py
+++ akips/ocnes.py
@@ -100,14 +100,14 @@ class EventManager:
             # Unreachable.objects.filter(status='Open', device__notify=False).summary_set.clear()
             ignored_unreachables = Unreachable.objects.filter(status='Open', device__notify=False)
             for ignored in ignored_unreachables:
-                logger.debug(f"unreachable {ignored} has notifications disabled, removing from all summaries")
+                logger.info(f"unreachable {ignored} has notifications disabled, removing from all summaries")
                 ignored.summary_set.clear()
 
             # Handle unreachables that have moved to hibernate mode
             # Unreachable.objects.filter(status='Open', device__hibernate=True).summary_set.clear()
             hibernated_unreachables = Unreachable.objects.filter(status='Open', device__hibernate=True)
             for hibernated in hibernated_unreachables:
-                logger.debug(f"unreachable {hibernated} has been set to hibernate, removing from all summaries")
+                logger.info(f"unreachable {hibernated} has been set to hibernate, removing from all summaries")
                 hibernated.summary_set.clear()
 
             # Remove stale entries
